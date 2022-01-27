fdcc7fd3d21bc908702d5c2191b43058bf7220ae
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-01-27 20:22:35 +0000

    Update

--- scripts/install_kvm_centos8.sh
+++ scripts/install_kvm_centos8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-dnf module install virt
-dnf install virt-install virt-viewer libguestfs-tools
+dnf module install virt -y
+dnf install -y virt-install virt-viewer libguestfs-tools
 dnf install -y git
 
 systemctl enable libvirtd
