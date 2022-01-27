575adeb875d444f2c9c25f63af361483cb0ba2ea
eslam-gomaa <eslam.sa3dany2015@gmail.com>
2022-01-27 21:50:53 +0000

    Add

---
+++ scripts/install_kvm_debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
+apt install -y git
+
+systemctl start libvirtd
+systemctl enable libvirtd
+systemctl is-active libvirtd
